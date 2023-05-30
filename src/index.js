9cfc3a77aab741caf5229cf4a6186a35fa574be3
beansdrawer <beansdrawer@naver.com>
2023-05-30 22:08:13 +0900

    uvc 리액트 단기 강의 준비

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Lecture06';
-import Task from './task/Task03';
+import App from './Lecture01';
+import Task from './task/Task01';
+import TodoApp from './todo/todo'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Task />
+    <TodoApp />
 );
